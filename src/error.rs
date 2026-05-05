e58294590d12053949dc088c65389535c34b46cd
rudolfmuller <rudolfmullercz@gmail.com>
2026-05-05 19:27:29 +0200

    added error enum 'error::ParseError', added wrapper for 'error::ParseError' as 'error::AudioError::Parse'

--- src/error.rs
+++ src/error.rs
@@ -2,6 +2,17 @@ use std::io::Error;
 use std::process::ExitStatus;
 use std::string::FromUtf8Error;
 
+use crate::lexer::Token;
+
+#[derive(thiserror::Error, Debug)]
+pub enum ParseError {
+    #[error("unexpected token: {unexpected:?}, expected: {expected:?}")]
+    UnexpectedToken { expected: Token, unexpected: Token },
+
+    #[error("the token: {0:?} cannot be found ")]
+    TokenNotFound(Token),
+}
+
 #[derive(thiserror::Error, Debug)]
 pub enum AudioError {
     #[error("failed to execute {program}: {err}")]
@@ -16,4 +27,7 @@ pub enum AudioError {
 
     #[error("invalid UTF-8")]
     InvalidUtf8(#[from] FromUtf8Error),
+
+    #[error(transparent)]
+    Parse(#[from] ParseError),
 }
